98605b0de19855d520a54b9b6b9148afdf493249
Karl Kustav Muldia <karlkustav26@gmail.com>
2022-10-23 19:14:59 +0300

    Added my functions to functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import os
 from settings import *
+import random
 
 #--------------------------------
 def flip():
@@ -44,3 +45,33 @@ def save_profile(name, money):
     with open("profiles.txt", "a+") as file:
         file.write("\n" + name + " " + str(money))
     file.close()
+
+def deal(target, amount):    # Chooses <amount> card(s) from deck at random, adds them to target and removes them from deck
+    global deck, player, player_total, dealer, dealer_total
+    x = []
+    for i in range(1, amount + 1):
+        y = deck[random.randint(1, len(deck) - 1)]
+        if y.value in range(2, 11):
+            player_total += y.value
+        elif y.value in range(11, 14):
+            player_total += 10
+        else:
+            if player_total + 11 > 21:
+                player_total += 1
+            else:
+                player_total += 11
+        x.append(y)
+        deck.remove(y)
+    target.extend(x)
+
+def generate_hands_deck():  # Generates shuffled deck and creates player and dealer hand lists
+    global deck, player, player_total, dealer, dealer_total
+    player = []
+    player_total = 0
+    dealer = []
+    dealer_total = 0
+    deck = []
+    suits = ['spades', 'clubs', 'hearts', 'diamonds']
+    card = namedtuple('card', ['value', 'suit'])
+    deck = list(card(value, suit) for suit in suits for value in range(1,14))
+    random.shuffle(deck)
